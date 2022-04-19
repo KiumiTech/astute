1811f21c3692ce37051ef8492db4bd01f28a0544
Yiming Lu <luyiming2009@gmail.com>
2022-04-19 18:59:25 +0800

    Initial commit

---
+++ src/main/java/com/kiumitech/astute/AstuteApplication.java
@@ -0,0 +1,13 @@
+package com.kiumitech.astute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstuteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstuteApplication.class, args);
+    }
+
+}
