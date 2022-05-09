c1fb801578f1a8efe56561e4b5581506d03c62c1
Yiming Lu <luyiming2009@gmail.com>
2022-05-09 14:27:41 +0800

    feat - Scheme endpoint implementation

--- src/main/java/com/kiumitech/astute/scheme/controller/SchemeController.java
+++ src/main/java/com/kiumitech/astute/scheme/controller/SchemeController.java
@@ -2,21 +2,28 @@ package com.kiumitech.astute.scheme.controller;
 
 import com.kiumitech.astute.basis.SingleResponse;
 import com.kiumitech.astute.scheme.dto.SchemeAddCmd;
+import com.kiumitech.astute.scheme.dto.SchemeUpdateCmd;
 import com.kiumitech.astute.scheme.dto.data.SchemeCO;
 import com.kiumitech.astute.scheme.service.SchemeApplicationService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/scheme")
 public class SchemeController {
     private final SchemeApplicationService schemeApplicationService;
 
+    @PostMapping
     public SingleResponse<SchemeCO> addScheme(@NonNull @RequestBody SchemeAddCmd cmd) {
         return schemeApplicationService.addScheme(cmd);
+    }
+
+    @PutMapping
+    public SingleResponse<SchemeCO> updateScheme(@NonNull @RequestBody SchemeUpdateCmd cmd) {
+        return schemeApplicationService.updateScheme(cmd);
     }
 }
